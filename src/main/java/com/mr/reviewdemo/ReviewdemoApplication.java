a0a067893db8d35b7a4cc03937aaee4171de34c0
edsion-ljj <1098389592@qq.com>
2020-04-19 21:41:05 +0800

    框架

---
+++ src/main/java/com/mr/reviewdemo/ReviewdemoApplication.java
@@ -0,0 +1,13 @@
+package com.mr.reviewdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mr/reviewdemo/ReviewdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr.reviewdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
